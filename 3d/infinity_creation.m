2286fb7bb75436f575b9963fd32dc0b389c2dd97
leadGuitarBG <95097787+BugraUckol@users.noreply.github.com>
2025-04-30 21:57:11 +0300

    [FIX] Fixed a bug regading the complex number assumption of the symbolic variable

--- 3d/infinity_creation.m
+++ 3d/infinity_creation.m
@@ -15,8 +15,15 @@ z = c * cos(t);
 
 % Plot
 figure;
+plot3(x, y, z, 'k', 'LineWidth', 2);
+    grid on; axis equal;
+    xlabel('x'); ylabel('y'); zlabel('z');
+    title('3D Infinity Curve (Non-Intersecting)');
+    view(135, 30); % adjust view angle for clarity
+    hold on
 
-syms t_s
+t_s = sym("t_s","real");
+
 xs = a * sin(t_s);
 ys = b * sin(2*t_s);
 zs = c * cos(t_s);
@@ -26,27 +33,25 @@ r_d = diff(r, t_s);
 
 ts = r_d / norm(r_d);
 
-ts_d = diff(ts, t_s);
+ts_d = simplify(diff(ts, t_s));
 
-ns = ts_d / norm(ts_d);
+ns = simplify(ts_d / norm(ts_d));
 
-bs = cross(ts, ns);
+bs = simplify(cross(ts, ns));
 
-for i = -100:1000
-    clf;
+for i = -10000:5:10000
+    cla;
     plot3(x, y, z, 'k', 'LineWidth', 2);
-    grid on; axis equal;
-    xlabel('x'); ylabel('y'); zlabel('z');
-    title('3D Infinity Curve (Non-Intersecting)');
-    view(135, 30); % adjust view angle for clarity
-    hold on
 
     ts_e = i * 2*pi / 1000;
     p = double(subs(r, t_s, ts_e));
+    
     try
+        tic
         t = double(subs(ts, t_s, ts_e));
         n = double(subs(ns, t_s, ts_e));
         b = double(subs(bs, t_s, ts_e));
+        toc
     end
     
     pt = [p, p + t];
@@ -56,6 +61,6 @@ for i = -100:1000
     plot3(pt(1,:), pt(2,:), pt(3,:), 'LineWidth', 2, 'Color', 'r');
     plot3(pn(1,:), pn(2,:), pn(3,:), 'LineWidth', 2, 'Color', 'g');
     plot3(pb(1,:), pb(2,:), pb(3,:), 'LineWidth', 2, 'Color', 'b');
-
-    pause(0.0005);
+    drawnow
+    toc
 end
