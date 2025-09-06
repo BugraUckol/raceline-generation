8988612fde80844db3e8aaa9a96bbfda5598f7fd
Buğra ÜÇKOL <bugrauckol@b1a5.wls.metu.edu.tr>
2025-09-06 23:00:27 +0300

    added sinusoid ptf etc

--- 3d/sinus_ptf.m
+++ 3d/sinus_ptf.m
@@ -13,17 +13,17 @@ b = 3;    % vertical amplitude
 c = 5;    % depth amplitude (3D deviation)
 
 % Parametric equations for 3D figure-eight curve
-x = a * (sin(t) + 2 * sin(2 * t));
-y = b * (cos(t) - 2 * cos(2 * t));
-z = c * ( - sin(3 * t));
+x = t;
+y = sin(t);
+z = 0*t;
 
 % Plot
 figure(1);
 
 t_s = sym("t_s","real");
-xs = a * (sin(t_s) + 2 * sin(2 * t_s));
-ys = b * (cos(t_s) - 2 * cos(2 * t_s));
-zs = c * ( - sin(3 * t_s));
+xs = t_s;
+ys = sin(t_s);
+zs = 0;
 
 r = [xs, ys, zs]';
 r_d = simplify(diff(r, t_s));
@@ -82,48 +82,52 @@ for i = 1:1:samples
     y_arr = [y_arr, p(2)];
     z_arr = [z_arr, p(3)];
 
-    kappa_arr = [kappa_arr, dtds * norm(double(subs(ts_d, t_s, ts_e)))];
-    tau_arr = [tau_arr, -dtds * dot(double(subs(bs_d, t_s, ts_e)), ...
-        double(subs(ns, t_s, ts_e)))];
-
-    s_arr = [s_arr, s_arr(end) + norm(p - p_prev)];
-
-    % Substitude parameter to obtain TNB vectors symbolically
-    try
-        t = double(subs(ts, t_s, ts_e));
-        n = double(subs(ns, t_s, ts_e));
-        b = double(subs(bs, t_s, ts_e));
-    catch
-        err_idx = [err_idx, ts_e];
-        plot3(p(1), p(2), p(3), 'o', 'MarkerSize', 5);
-    end
-
-    % Create TNB Frame from local tangent and derivatives of it
-    pt = [p, p + 0.5 * t];
-    pn = [p, p + 0.5 * n];
-    pb = [p, p + 0.5 * b];
-
+    % kappa_arr = [kappa_arr, dtds * norm(double(subs(ts_d, t_s, ts_e)))];
+    % tau_arr = [tau_arr, -dtds * dot(double(subs(bs_d, t_s, ts_e)), ...
+    %     double(subs(ns, t_s, ts_e)))];
+    % 
+    % s_arr = [s_arr, s_arr(end) + norm(p - p_prev)];
+    % 
+    % % Substitude parameter to obtain TNB vectors symbolically
+    % try
+    %     t = double(subs(ts, t_s, ts_e));
+    %     n = double(subs(ns, t_s, ts_e));
+    %     b = double(subs(bs, t_s, ts_e));
+    % catch
+    %     err_idx = [err_idx, ts_e];
+    %     plot3(p(1), p(2), p(3), 'o', 'MarkerSize', 5);
+    % end
+    % 
+    % % Create TNB Frame from local tangent and derivatives of it
+    % pt = [p, p + 0.5 * t];
+    % pn = [p, p + 0.5 * n];
+    % pb = [p, p + 0.5 * b];
+    % 
     tt_arr = [tt_arr, t];
-    nn_arr = [nn_arr, n];
-    bb_arr = [bb_arr, b];
-
-    [yaw, pitch, roll] = dcm2angle( [t,n,b]', 'zyx', 'robust');
-
-    yaw_arr = [yaw_arr, yaw];
-    pitch_arr = [pitch_arr, pitch];
-    roll_arr = [roll_arr, roll];
-
+    % nn_arr = [nn_arr, n];
+    % bb_arr = [bb_arr, b];
+    % 
+    % [yaw, pitch, roll] = dcm2angle( [t,n,b]', 'zyx', 'robust');
+    % 
+    % yaw_arr = [yaw_arr, yaw];
+    % pitch_arr = [pitch_arr, pitch];
+    % roll_arr = [roll_arr, roll];
+    % 
     % Frenet-Serret
     omega = cross(double(subs(r_d, t_s, ts_e)), ...
         double(subs(r_dd, t_s, ts_e))) / norm(double(subs(r_d, t_s, ts_e)))^2;
     % dcm_prime = skew(omega) * [tt, nn, bb];
-    
     omega_p = [tt, nn, bb]' * omega;
     omega_p(1) = 0
     dcm_prime = [tt, nn, bb] * skew([tt, nn, bb]' * omega);
     tt = tt + dt * dcm_prime(:,1);
     nn = nn + dt * dcm_prime(:,2);
     bb = bb + dt * dcm_prime(:,3);
+
+    % dcm_prime = skew(omega) * [tt, nn, bb];
+    % tt = tt + dt * dcm_prime(:,1);
+    % nn = nn + dt * dcm_prime(:,2);
+    % bb = bb + dt * dcm_prime(:,3);
 
     % tt = t;
     % null_t = null(t');
@@ -135,7 +139,7 @@ for i = 1:1:samples
     pnn = [p, p + nn];
     pbb = [p, p + bb];
 
-    if i == 1
+    if i == 19999999
         pt = [p, p + 1.5 * t];
         pn = [p, p + 1.5 * n];
         pb = [p, p + 1.5 * b];
@@ -149,12 +153,12 @@ for i = 1:1:samples
         % plot3(pn(1,:), pn(2,:), pn(3,:), 'LineWidth', 2, 'Color', 'g');
         % plot3(pb(1,:), pb(2,:), pb(3,:), 'LineWidth', 2, 'Color', 'b');
 
-        % plot3(ptt(1,:), ptt(2,:), ptt(3,:), 'LineWidth', 2, 'Color', 'r', ...
-        %     'LineStyle', '-.');
-        % plot3(pnn(1,:), pnn(2,:), pnn(3,:), 'LineWidth', 2, 'Color', 'g', ...
-        %     'LineStyle', '-.');
-        % plot3(pbb(1,:), pbb(2,:), pbb(3,:), 'LineWidth', 2, 'Color', 'b', ...
-        %     'LineStyle', '-.');
+        plot3(ptt(1,:), ptt(2,:), ptt(3,:), 'LineWidth', 2, 'Color', 'r', ...
+            'LineStyle', '-.');
+        plot3(pnn(1,:), pnn(2,:), pnn(3,:), 'LineWidth', 2, 'Color', 'g', ...
+            'LineStyle', '-.');
+        plot3(pbb(1,:), pbb(2,:), pbb(3,:), 'LineWidth', 2, 'Color', 'b', ...
+            'LineStyle', '-.');
     end
     pause(0.005);
     p_prev = p;
@@ -163,7 +167,7 @@ s_arr = s_arr(2:end);
 
 figure(2)
 plot(s_arr, kappa_arr, 'LineWidth', 4);
-hold all
+hold on
 plot(s_arr, tau_arr, 'LineWidth', 4);
 xlabel('Curvilinear Distance [m]')
 ylabel('Value [1/m]')
