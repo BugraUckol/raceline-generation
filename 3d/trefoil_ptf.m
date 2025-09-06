d6352d219de55e7af21905d5614256df1a3e8baf
Buğra ÜÇKOL <bugrauckol@b1a5.wls.metu.edu.tr>
2025-09-07 00:01:56 +0300

    improved ptf based approach

--- 3d/trefoil_ptf.m
+++ 3d/trefoil_ptf.m
@@ -31,20 +31,13 @@ r_dd = simplify(diff(r_d, t_s));
 
 ts = simplify(r_d / norm(r_d));
 
-ts_d = simplify(diff(ts, t_s));
-
-ns = simplify(ts_d / norm(ts_d));
-
-bs = simplify(cross(ts, ns));
-
-bs_d = simplify(diff(bs, t_s));
-
 start = eps;
 err_idx = [];
 x_arr = [];
 y_arr = [];
 z_arr = [];
-kappa_arr = [];
+kap1_arr = [];
+kap2_arr = [];
 tau_arr = [];
 s_arr = [0];
 tt_arr = [];
@@ -81,97 +74,72 @@ for i = 1:1:samples
     x_arr = [x_arr, p(1)];
     y_arr = [y_arr, p(2)];
     z_arr = [z_arr, p(3)];
-
-    kappa_arr = [kappa_arr, dtds * norm(double(subs(ts_d, t_s, ts_e)))];
-    tau_arr = [tau_arr, -dtds * dot(double(subs(bs_d, t_s, ts_e)), ...
-        double(subs(ns, t_s, ts_e)))];
-
     s_arr = [s_arr, s_arr(end) + norm(p - p_prev)];
 
-    % Substitude parameter to obtain TNB vectors symbolically
-    try
-        t = double(subs(ts, t_s, ts_e));
-        n = double(subs(ns, t_s, ts_e));
-        b = double(subs(bs, t_s, ts_e));
-    catch
-        err_idx = [err_idx, ts_e];
-        plot3(p(1), p(2), p(3), 'o', 'MarkerSize', 5);
-    end
-
-    % Create TNB Frame from local tangent and derivatives of it
-    pt = [p, p + 0.5 * t];
-    pn = [p, p + 0.5 * n];
-    pb = [p, p + 0.5 * b];
-
-    tt_arr = [tt_arr, t];
-    nn_arr = [nn_arr, n];
-    bb_arr = [bb_arr, b];
-
-    [yaw, pitch, roll] = dcm2angle( [t,n,b]', 'zyx', 'robust');
-
-    yaw_arr = [yaw_arr, yaw];
-    pitch_arr = [pitch_arr, pitch];
-    roll_arr = [roll_arr, roll];
-
-    % Frenet-Serret
+    % No torsion angular velocity
     omega = cross(double(subs(r_d, t_s, ts_e)), ...
         double(subs(r_dd, t_s, ts_e))) / norm(double(subs(r_d, t_s, ts_e)))^2;
     % dcm_prime = skew(omega) * [tt, nn, bb];
     
     omega_p = [tt, nn, bb]' * omega;
-    omega_p(1) = 0
-    dcm_prime = [tt, nn, bb] * skew([tt, nn, bb]' * omega);
+    omega_p(1) = 0;
+    % dcm_prime = [tt, nn, bb] * skew([tt, nn, bb]' * omega);
+    dcm_prime = skew(omega) * [tt, nn, bb];
     tt = tt + dt * dcm_prime(:,1);
     nn = nn + dt * dcm_prime(:,2);
     bb = bb + dt * dcm_prime(:,3);
 
-    % tt = t;
-    % null_t = null(t');
-    % nn = null_tt(:,1);
-    % bb = null_tt(:,2);
+    [yaw, pitch, roll] = dcm2angle( [tt,nn,bb]', 'zyx', 'robust');
+    yaw_arr = [yaw_arr, yaw];
+    pitch_arr = [pitch_arr, pitch];
+    roll_arr = [roll_arr, roll];
+    tt_arr = [tt_arr, tt];
+    nn_arr = [nn_arr, nn];
+    bb_arr = [bb_arr, bb];
 
     % Create TNB Frame with Frenet-Serret
-    ptt = [p, p + tt];
-    pnn = [p, p + nn];
-    pbb = [p, p + bb];
+    pt = [p, p + tt];
+    pn = [p, p + nn];
+    pb = [p, p + bb];
 
-    if i == 1
-        pt = [p, p + 1.5 * t];
-        pn = [p, p + 1.5 * n];
-        pb = [p, p + 1.5 * b];
-        plot3(pt(1,:), pt(2,:), pt(3,:), 'LineWidth', 4, 'Color', 'r');
-        plot3(pn(1,:), pn(2,:), pn(3,:), 'LineWidth', 4, 'Color', 'g');
-        plot3(pb(1,:), pb(2,:), pb(3,:), 'LineWidth', 4, 'Color', 'b');
-        [spx, spy, spz] = sphere(10);
-        surf(p(1) + 0.5*spx, p(2) + 0.5*spy, p(3) + 0.5*spz, 'FaceAlpha',0.5, 'FaceColor', 'r');
-    elseif mod(i,5) == 0
-        % plot3(pt(1,:), pt(2,:), pt(3,:), 'LineWidth', 2, 'Color', 'r');
-        % plot3(pn(1,:), pn(2,:), pn(3,:), 'LineWidth', 2, 'Color', 'g');
-        % plot3(pb(1,:), pb(2,:), pb(3,:), 'LineWidth', 2, 'Color', 'b');
-
-        % plot3(ptt(1,:), ptt(2,:), ptt(3,:), 'LineWidth', 2, 'Color', 'r', ...
-        %     'LineStyle', '-.');
-        % plot3(pnn(1,:), pnn(2,:), pnn(3,:), 'LineWidth', 2, 'Color', 'g', ...
-        %     'LineStyle', '-.');
-        % plot3(pbb(1,:), pbb(2,:), pbb(3,:), 'LineWidth', 2, 'Color', 'b', ...
-        %     'LineStyle', '-.');
+    if mod(i,15) == 0
+        plot3(pt(1,:), pt(2,:), pt(3,:), 'LineWidth', 2, 'Color', 'r');
+        plot3(pn(1,:), pn(2,:), pn(3,:), 'LineWidth', 2, 'Color', 'g');
+        plot3(pb(1,:), pb(2,:), pb(3,:), 'LineWidth', 2, 'Color', 'b');
     end
     pause(0.005);
     p_prev = p;
 end
 s_arr = s_arr(2:end);
 
+for i = 1:1:samples
+    ts_e = i * 2 * pi / samples + start;
+    dt = 2 * pi / samples;
+    p = double(subs(r, t_s, ts_e));
+    ds = norm(p - p_prev);
+    % No torsion angular velocity
+    omega = cross(double(subs(r_d, t_s, ts_e)), ...
+        double(subs(r_dd, t_s, ts_e))) / norm(double(subs(r_d, t_s, ts_e)))^2;
+    % dcm_prime = skew(omega) * [tt, nn, bb];
+    tt = tt_arr(:,i);
+    nn = nn_arr(:,i);
+    bb = bb_arr(:,i);
+    omega_p = [tt, nn, bb]' * omega;
+    kap1_arr = [kap1_arr, dtds * omega(2,1)];
+    kap2_arr = [kap2_arr, dtds * omega(3,1)];
+end
+
 figure(2)
-plot(s_arr, kappa_arr, 'LineWidth', 4);
+plot(s_arr, kap1_arr, 'LineWidth', 4);
 hold all
-plot(s_arr, tau_arr, 'LineWidth', 4);
+plot(s_arr, kap2_arr, 'LineWidth', 4);
 xlabel('Curvilinear Distance [m]')
 ylabel('Value [1/m]')
-legend('Curvature','Torsion')
-title('Arclength -vs- Curvature and Torsion Profile of The Trefoil')
+legend('$$\kappa_1$$','$$\kappa_2$$','interpreter','latex')
+title('Arclength -vs- Generalized Curvature Profiles of The Curve')
 grid minor
 
 display(strcat('Problematic points are', 20, num2str(err_idx')))
 
-% % % save trefoil s_arr x_arr y_arr z_arr kappa_arr tau_arr tt_arr ...
-% % %     nn_arr bb_arr yaw_arr pitch_arr roll_arr
+save trefoil_ptf s_arr x_arr y_arr z_arr kap1_arr kap2_arr tt_arr ...
+    nn_arr bb_arr yaw_arr pitch_arr roll_arr
